2196bca30bf627c45e3d427518b8575d403992d9
vpnshopee <90919599+shopeevpn@users.noreply.github.com>
2022-01-12 23:24:39 +0800

    Create fix-update.sh

---
+++ fix-update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /usr/bin
+echo "[ Info ] Update version menu"
+echo "[ Creating ] Update version menu"
+echo "[ Error ] Update version menu"
+echo "[ ok ] Update version menu"
+echo "[ connected ] Update version menu"
+echo "[ Checking ] Update version menu"
+echo "[ Invalid ] Update version menu"
+echo "[ ERROR ] Update version menu"
+echo "[ Information ] wget -O menu"
+#wget -O menu "https://raw.githubusercontent.com/shopeevpn/V2XRAY-V2/main/menu.sh"
+echo "[ Information ] chmod +x menu"
+chmod +x menu
+cd
